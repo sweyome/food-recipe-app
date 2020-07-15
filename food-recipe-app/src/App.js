6e25bdead6af44f2384d3b9b435910c114c33d7d
sweyome <sweyome1@gmail.com>
2020-07-15 18:59:45 -0400

    enviromet for API key

--- food-recipe-app/src/App.js
+++ food-recipe-app/src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  
-  const APP_ID = 'af3feda2'
-  const APP_KEY = '365f22c218287bd118bc726a86c08651'
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+  const APP_ID = process.env.APP_ID
+  const APP_KEY = process.env.APP_KEY
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
 
 
