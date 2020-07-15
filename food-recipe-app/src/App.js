32b21b8bf087c10bf6321b8f14217da4b19cc406
sweyome <sweyome1@gmail.com>
2020-07-15 18:39:50 -0400

    app setup done

--- food-recipe-app/src/App.js
+++ food-recipe-app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  
+  const APP_ID = 'af3feda2'
+  const APP_KEY = '365f22c218287bd118bc726a86c08651'
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello React</h1>
     </div>
-  );
+  )
 }
 
 export default App;
